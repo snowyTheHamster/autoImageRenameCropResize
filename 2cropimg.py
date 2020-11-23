0fb149e44d64e1335bf761f5a986f43ebda3f73d
taku <vidafuku@gmail.com>
2020-11-23 16:15:07 +0800

    separated 2crop function as individual gui app

--- 2cropimg.py
+++ 2cropimg.py
@@ -8,36 +8,18 @@ import PySimpleGUI as sg
 
 image_exts = [ '.jpg', '.jpeg' ]
 
-
-def folder_init():
-    # create temp folders
-    try:
-        os.mkdir(CROPPED_DIR)
-        os.mkdir(CROPPED_DIR_KAGE)
-    except:
-        print('directory already exists')
-
-
-def cropitup():
-    cropped_save_folders = [CROPPED_DIR_KAGE, CROPPED_DIR]
-
-    MIN_VAL = 210
-    MAX_VAL = 200
-
-    #Products Margins within Rectangle
-    LEFT = 0 + MLEFT
-    TOP = 200 + MTOP
-    RIGHT = 0 + MRIGHT
-    BOTTOM = 220
+def cropitup(mode="duel"):
+    folders_to_crop = [INPUT_DIR, ORIGINAL_DIR]
+    cropped_save_folders = [CROPPED_DIR, CROPPED_DIR_KAGE]
 
     # Iterate over working directory
-    for crop_save in cropped_save_folders:
+    for crop_folder, crop_save in zip(folders_to_crop, cropped_save_folders):
         count = 0
-        for _, _, files in os.walk(REFERENCE_DIR):
+        for _, _, files in os.walk(INPUT_DIR):
             file_count = len(files)
             for filename in files:
                 count +=1
-                file_path = os.path.join(REFERENCE_DIR, filename)
+                file_path = os.path.join(INPUT_DIR, filename)
                 file_name, file_ext = os.path.splitext(file_path)
                 output_file_name = os.path.basename(file_name) + file_ext # save imagename (change ext if you want)
 
@@ -48,9 +30,7 @@ def cropitup():
                     continue
 
                 else:
-                    print(f'{count}/{file_count} Cropping {filename}...')
-
-                    image = cv2.imread(REFERENCE_DIR+'/'+filename)
+                    image = cv2.imread(INPUT_DIR+'/'+filename)
                     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
                     ret, thresh = cv2.threshold(gray, MIN_VAL, MAX_VAL, cv2.THRESH_BINARY_INV) # options: THRESH_BINARY,THRESH_BINARY_INV,THRESH_TRUNC,THRESH_TOZERO,THRESH_TOZERO_INV
@@ -70,16 +50,16 @@ def cropitup():
                     # Make a rectangle from the largest coutour
                     x,y,w,h = cv2.boundingRect(c)
                     # Set margins for the Rectangle to prevent clipping of product
-                    x = x - LEFT
-                    y = y - TOP
-                    w = w + RIGHT
-                    h = h + BOTTOM
+                    x = x - MLEFT
+                    y = y - MTOP
+                    w = w + MLEFT + MRIGHT
+                    h = h + MTOP + MBOTTOM
 
                     # draw the biggest contour (c) in white(255) with 1px border
                     image = cv2.rectangle(image,(x,y),(x+w,y+h),(255, 255, 255), 1)
                     
                     # Select which Images to apply changes to
-                    original_image = cv2.imread(INPUT_DIR +'/'+ filename) # Apply crops to images in INPUT_DIR
+                    original_image = cv2.imread(crop_folder +'/'+ filename) # Apply crops to images in INPUT_DIR
                 
                     # crop the image
                     cropped_image = original_image[y:y+h, x:x+w]
@@ -87,12 +67,23 @@ def cropitup():
                     # Save image to output dir
                     cv2.imwrite(crop_save +'/'+ output_file_name, cropped_image)
 
+                    print(f'{count}/{file_count} Cropping {filename}... done.')
+
+        # If Original Images Folder not selected, dont process second time
+        if mode == 'single':
+            break
+
 
 layout = [
-    [sg.Text('Input folder:')],
+    [sg.Text('Original Images folder (Optional):')],
+    [sg.Input(key='_ORIGINAL_IMG_'), sg.FolderBrowse()],
+    [sg.Text('Bg removed Images folder:')],
     [sg.Input(key='_IN_IMG_'), sg.FolderBrowse()],
     [sg.Text('Output folder:')],
     [sg.Input(key='_OUT_IMG_'), sg.FolderBrowse()],
+    [sg.Text('--- Threshold Values ---')],
+    [sg.Text('Min Value:'), sg.Slider(key='_MIN_VALUE_', range=(0,255), default_value=210, size=(35,15), orientation='horizontal')],
+    [sg.Text('Max Value:'), sg.Slider(key='_MAX_VALUE_', range=(0,255), default_value=200, size=(35,15), orientation='horizontal')],
     [sg.Text('')],
     [sg.Text('Additional Margins (px)')],
     [sg.Text('Top:'), sg.InputText(0, key='_MARGIN_TOP_', size=(5,1)), sg.Text('Right:'), sg.InputText(0, key='_MARGIN_RIGHT_', size=(5,1)), sg.Text('Bottom:'), sg.InputText(0, key='_MARGIN_BOTTOM_', size=(5,1)), sg.Text('Left:'), sg.InputText(0, key='_MARGIN_LEFT_', size=(5,1))],
@@ -108,10 +99,14 @@ try:
         if event is None:
             break
         if event == '_PROCESS_':
+            ORIGINAL_DIR = values['_ORIGINAL_IMG_']
             INPUT_DIR = values['_IN_IMG_']
             OUTPUT_DIR = values['_OUT_IMG_']
             CROPPED_DIR = os.path.join(OUTPUT_DIR, '2_cropped')
             CROPPED_DIR_KAGE = os.path.join(OUTPUT_DIR, '2_cropped_helper')
+
+            MIN_VAL = int(values['_MIN_VALUE_'])
+            MAX_VAL = int(values['_MAX_VALUE_'])
 
             MTOP = int(values['_MARGIN_TOP_'])
             MRIGHT = int(values['_MARGIN_RIGHT_'])
@@ -123,13 +118,23 @@ try:
                 print('please specify folders')
             elif OUTPUT_DIR == INPUT_DIR:
                 print('Output Folder cannot be same as the Input Folder')
-            elif os.listdir(CROPPED_DIR) :
-                print('2_cropped Folder must be Empty')
-            elif os.listdir(CROPPED_DIR_KAGE) :
-                print('2_cropped_helper Folder must be Empty')
+            elif os.listdir(OUTPUT_DIR) :
+                print('Output Folder must be Empty')
             else:
-                folder_init()
-                cropitup()
+                try:
+                    shutil.rmtree(CROPPED_DIR, ignore_errors=True)
+                    shutil.rmtree(CROPPED_DIR_KAGE, ignore_errors=True)
+                    os.mkdir(CROPPED_DIR)
+                    os.mkdir(CROPPED_DIR_KAGE)
+                except:
+                    print('directory already exists')
+
+                # if Original Images folder empty
+                if ORIGINAL_DIR == '':
+                    cropitup("single") # run once
+                else:
+                    cropitup() # run twice
+
 
 except Exception as e:
     tb = traceback.format_exc()
